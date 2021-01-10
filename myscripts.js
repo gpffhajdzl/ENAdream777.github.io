be8e2c983d47bed4e7fbb67dd49c7eeb133f837f
ENAdream777 <58988261+ENAdream777@users.noreply.github.com>
2021-01-10 07:12:38 -0800

    ver 0.5 beta
    this is a beta version of the E&A website

---
+++ myscripts.js
@@ -0,0 +1,19 @@
+var btn2 = document.getElementById("github");
+
+function about() {
+    location.href = "about.html";
+}
+
+function univTest() {
+    location.href = "univTest.html";
+}
+
+function successStory() {
+    location.href = "sucessStory.html";
+}
+
+function contact() {
+    location.href = "http://pf.kakao.com/_ptbIxb/chat";
+}
+
+
